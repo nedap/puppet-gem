6e54d020be8d8386fd51992a8ed5bf01161645db
Robin F. Pronk <robin.pronk@nedap.com>
2026-03-01 21:50:15 +0100

    Use systemd on trixie

--- lib/puppet/provider/service/systemd.rb
+++ lib/puppet/provider/service/systemd.rb
@@ -8,7 +8,7 @@ Puppet::Type.type(:service).provide :systemd, :parent => :base do
   defaultfor :osfamily => [:archlinux]
   defaultfor :osfamily => :redhat, :operatingsystemmajrelease => "7"
   defaultfor :osfamily => :redhat, :operatingsystem => :fedora, :operatingsystemmajrelease => ["17", "18", "19", "20", "21"]
-  defaultfor :operatingsystem => :debian, :operatingsystemmajrelease => ["9", "10", "11", "12"]
+  defaultfor :operatingsystem => :debian, :operatingsystemmajrelease => ["9", "10", "11", "12", "13"]
 
   def self.instances
     i = []
